9c6211012134fba5cccfdbff6f6abb9a90c3eacf
nikolaos karapas <nikos.karapas86@gmail.com>
2021-12-03 20:27:22 +0200

    timers in class - functions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Clock_cl from './clock-cl';
+import Clock_fn from './clock-fn';
 
 function App() {
   return (
     <div className="App">
       <Clock_cl/>
+      <Clock_fn/>
     </div>
   );
 }
--- src/clock-fn.js
+++ src/clock-fn.js
@@ -1,2 +1,19 @@
+import { useEffect, useRef, useState } from "react";
+function Clock_fn() {
+  const [date, setDate] = useState(new Date());
+  // const
+  const intervalId = useRef(null);
 
-
+  useEffect(() => {
+    //   console.log('use Effect')
+    intervalId.current = setInterval(() => setDate(new Date()), 1000);
+    return () => clearInterval(intervalId.current);
+  }, [date]);
+  return (
+    <>
+      <h1>Clock in functional component</h1>
+      <div>It is {date.toLocaleTimeString()}</div>
+    </>
+  );
+}
+export default Clock_fn;
