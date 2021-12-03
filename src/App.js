55980810f6ebf171ba99b53ad76f1bf34b138d85
nikolaos karapas <nikos.karapas86@gmail.com>
2021-12-03 20:47:39 +0200

    router added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-
-import { BrowserRouter , Route, Switch } from 'react-router-dom';
-import './App.css';
-import Clock_cl from './clock-cl';
-import Clock_fn from './clock-fn';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Clock_cl from "./clock-cl";
+import Clock_fn from "./clock-fn";
 
 function App() {
   return (
-    <div className="App">
-      {/* <Clock_cl/>
-      <Clock_fn/> */}
-<BrowserRouter>
-<Switch>
-  <Route exact path='/class' component={Clock_cl}/>
-  <Route exact  path='/function' component={Clock_fn}/>
-</Switch>
-</BrowserRouter>
-
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/class" element={<Clock_cl/>} />
+          <Route exact path="/function" element={<Clock_fn/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
