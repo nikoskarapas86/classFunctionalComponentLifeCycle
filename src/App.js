11bae691b1cb158f458faa463f91b845933ea72d
nikolaos karapas <nikos.karapas86@gmail.com>
2021-12-03 20:34:15 +0200

    router added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter , Route, Switch } from 'react-router-dom';
 import './App.css';
 import Clock_cl from './clock-cl';
 import Clock_fn from './clock-fn';
