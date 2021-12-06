c1ab36647d90a0e5545a544e450bc58e457f9e5b
nikolaos karapas <nikos.karapas86@gmail.com>
2021-12-06 21:42:31 +0200

    delete person

--- src/PersonRemove.js
+++ src/PersonRemove.js
@@ -1,34 +1,70 @@
 import axios from "axios";
-import { useState } from "react";
-import { useNavigate } from "react-router";
+import { Component } from "react";
 
-
-export default function PersonRemove(){
-    const [id, setId] = useState({});
-    const navigate = useNavigate();
-    const onSubmitForm = (event) => {
-      event.preventDefault();
-
-      axios
-        .delete(`https://jsonplaceholder.typicode.com/users/${id}`)
-        .then((res) => {
-            console.log(res)
-          if(res.status ==200){
-              navigate("/personList/persons")
-          }
-          });
+export default class PersonRemove extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: 0,
     };
-  
+    this.handleChange = this.handleChange.bind(this);
+    this.onSubmitForm = this.onSubmitForm.bind(this);
+  }
+  onSubmitForm(event) {
+    event.preventDefault();
+
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/users/${this.state.id}`)
+      .then((res) => {
+        console.log(res);
+       
+      });
+  }
+  handleChange(event) {
+    this.setState({ id: event.target.value });
+  }
+ 
+  render() {
     return (
-      <form onSubmit={onSubmitForm}>
+      <form onSubmit={this.onSubmitForm}>
         <label>id</label>
-        <input
-          type="number"
-          name="id"
-          onChange={(event) => setId(event.target.value)}
-        />
+        <input type="number" name="id" value={this.state.id} onChange={this.handleChange} />
         <button type="submit">DELETE</button>
       </form>
     );
-
+  }
 }
+
+// import axios from "axios";
+// import { useState } from "react";
+// import { useNavigate } from "react-router";
+
+// export default function PersonRemove(){
+//     const [id, setId] = useState({});
+//     const navigate = useNavigate();
+//     const onSubmitForm = (event) => {
+//       event.preventDefault();
+
+//       axios
+//         .delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+//         .then((res) => {
+//             console.log(res)
+//           if(res.status ==200){
+//               navigate("/personList/persons")
+//           }
+//           });
+//     };
+
+//     return (
+//       <form onSubmit={onSubmitForm}>
+//         <label>id</label>
+//         <input
+//           type="number"
+//           name="id"
+//           onChange={(event) => setId(event.target.value)}
+//         />
+//         <button type="submit">DELETE</button>
+//       </form>
+//     );
+
+// }
